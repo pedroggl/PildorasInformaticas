2da470c3c34798fad913277dfbbab5ad366c144c
pedroggl <gallardolozanopedrogabriel@gmail.com>
2023-06-07 12:18:51 +0200

    Primer commit, JavaSwing cap55-56

---
+++ javaSwing/src/main/java/graficos/JavaSwing.java
@@ -0,0 +1,37 @@
+package graficos;
+
+/**
+ *
+ * @author Pedro G Gallardo
+ */
+import javax.swing.*;
+
+public class JavaSwing {
+
+    public static void main(String[] args) {
+        miMarco frame1 = new miMarco();
+        frame1.setVisible(true);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class miMarco extends JFrame {
+
+    public miMarco() {
+        //inicia tamaño y posicion
+//        setSize(500, 500);
+//        setLocation(400, 100);
+
+        //genera posicion X, Y y tamaño ancho y alto
+        setBounds(400, 100, 500, 500);
+        
+        //pone un titulo a la ventana
+        setTitle("Vntana Grafica: Pedros`");
+        
+        //no permite que el marco o frame se redimensione
+//        setResizable(false);
+
+        //permite que el marco se amplie a pantalla completa
+//        setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+}
---
+++ javaSwing/src/main/java/graficos/MarcoCentrado.java
@@ -0,0 +1,18 @@
+package graficos;
+
+/**
+ *
+ * @author Pedro G Gallardo
+ */
+import java.awt.*;
+import javax.swing.*;
+
+public class MarcoCentrado extends JFrame {
+
+    //crea un objeto con el sistema nativo de ventana
+    Toolkit miPantalla = Toolkit.getDefaultToolkit();
+
+    //metodo que devuelve una clase dimension con el tamaño de mi pantalla
+    Dimension tamañoPantalla = miPantalla.getScreenSize();
+
+}
